91e4eeb6cdb667213ec00e55d1385230b68be55c
Hushibul <nayeemninz786@gmail.com>
2025-04-08 11:24:44 +0600

    pm2 restart modification

--- pm2_mongo_restart_service.sh
+++ pm2_mongo_restart_service.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-if pm2 status &> /dev/null; then
-    echo "PM2 is running.";
-else
-    sudo pm2 stop all;
-    sudo pm2 start /var/www/no_crop_comphy_api/dist/index.js --name comphy-admin-api;
-    echo "PM2 restarted by pm2_mongo_restart_service.";
+# Load NVM and Node.js
+export NVM_DIR="$HOME/.nvm"
+source "$NVM_DIR/nvm.sh"
+nvm use 22.14.0
 
+# Define full path to PM2
+PM2_BIN="$(which pm2)"
+
+# Check PM2 process status
+pm2_status=$($PM2_BIN status "stat_viewer" | grep "stat_viewer" | awk '{print $18}' | tr -d '[:space:]')
+
+if [[ "$pm2_status" == "stopped" ]]; then
+    echo "PM2 Restarting"
+    $PM2_BIN stop all
+    $PM2_BIN start /var/www/html/stat-viewer/app.js --name stat_viewer
+    echo "PM2 restarted by pm2_mongo_restart_service."
+else 
+    echo "PM2 running"
 fi
 
-# Check if MongoDB service (mongod) is running
-if pgrep -x "mongod" > /dev/null;
-then
-    echo "MongoDB service is running.";
+# Check if MongoDB is running
+if pgrep -x "mongod" > /dev/null; then
+    echo "MongoDB service is running."
 else
-    sudo service mongod restart;
-    echo "MongoDB restarted by pm2_mongo_restart_service.";
+    sudo systemctl restart mongod
+    echo "MongoDB restarted by pm2_mongo_restart_service."
 fi
--- remove_service.sh
+++ remove_service.sh
@@ -4,9 +4,7 @@ service_name=$(jq -r '.name' config.json)
 
 systemctl stop $service_name
 systemctl disable $service_name
-rm /etc/systemd/system/$service_name
 rm /etc/systemd/system/$service_name # and symlinks that might be related
-rm /usr/lib/systemd/system/$service_name 
 rm /usr/lib/systemd/system/$service_name # and symlinks that might be related
 systemctl daemon-reload
 systemctl reset-failed
